e261ebc13f7788ddbc73e66b7ea26325f5b785b6
MiiShot <kidrakon838084@gmail.com>
2025-01-28 14:02:10 +0700

    ห

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\HomeController;
 
+Route::get('/myview', function () {
+    return view('myview');
+});
 
 Route::get('/', function () {
     return view('layouts.default');
