de70b153074ddb1b40bd856f39eee81bdf7b07df
MiiShot <kidrakon838084@gmail.com>
2025-01-29 21:05:09 +0700

    d

--- mylaravel/resources/views/login.blade.php
+++ mylaravel/resources/views/login.blade.php
@@ -4,26 +4,29 @@
 <div class="login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../index2.html"><b>Admin</b>LTE</a>
+            <a href="{{ url('/') }}"><b>Admin</b>LTE</a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
             <div class="card-body login-card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
-                <form action="../index3.html" method="post">
+                
+                <form action="{{ route('login') }}" method="POST">
+                    @csrf
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email" />
+                        <input type="email" name="email" class="form-control" placeholder="Email" required />
                         <div class="input-group-text"><span class="bi bi-envelope"></span></div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password" />
+                        <input type="password" name="password" class="form-control" placeholder="Password" required />
                         <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
                     </div>
+                    
                     <!--begin::Row-->
                     <div class="row">
                         <div class="col-8">
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                <input class="form-check-input" type="checkbox" name="remember" id="flexCheckDefault" />
                                 <label class="form-check-label" for="flexCheckDefault"> Remember Me </label>
                             </div>
                         </div>
@@ -37,6 +40,7 @@
                     </div>
                     <!--end::Row-->
                 </form>
+
                 <div class="social-auth-links text-center mb-3 d-grid gap-2">
                     <p>- OR -</p>
                     <a href="#" class="btn btn-primary">
@@ -47,9 +51,10 @@
                     </a>
                 </div>
                 <!-- /.social-auth-links -->
-                <p class="mb-1"><a href="forgot-password.html">I forgot my password</a></p>
+
+                <p class="mb-1"><a href="{{ route('password.request') }}">I forgot my password</a></p>
                 <p class="mb-0">
-                    <a href="register.html" class="text-center"> Register a new membership </a>
+                    <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
                 </p>
             </div>
             <!-- /.login-card-body -->
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,27 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\MyController;
+use App\Http\Controllers\LaravelFormController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\HomeController;
 
-// หน้าแรก -> ไปที่ home
-Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// Dashboard หรือหน้าหลัก
-Route::get('/home', [HomeController::class, 'index'])->name('dashboard');
+Route::get('/login',
+    [LoginController::class, 'index']);
 
-// หน้าล็อกอิน
-Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/register',
+    [RegisterController::class, 'index']);
 
-// หน้าสมัครสมาชิก
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::get('/home',
+    [HomeController::class, 'index']);
 
-// สูตรคูณ
-Route::get('/multiplication', [MyController::class, 'back']);
-Route::post('/multiplication', [MyController::class, 'multiplication']);
+Route::get('/',
+    [HomeController::class, 'index']);
 
-// ระบบ Auth ของ Laravel (login, logout, register)
-Auth::routes();
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
