072e3bc0986547f959ea503dfd38d57867166412
Miishot <kidrakon838084@gmail.com>
2025-01-29 15:57:27 +0700

    hkjh

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login',
+    [LoginController::class, 'index'])
 
-Route::get('/hello', function () {
+Route::get('/register',
+    [RegisterController::class, 'index']);
+
+Route::get('/home',
+    [HomeController::class, 'index']);
+
+Route::get('/',
+    [HomeController::class, 'index']);
+
+Route::get('/hello', function () {s
     return "<h1>hello world!</h1>";
 });
