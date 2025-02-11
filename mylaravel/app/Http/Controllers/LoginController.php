cc32be8a533ad249d43be15b694c13bd6efe3160
MiiShot <kidrakon838084@gmail.com>
2025-02-11 20:07:48 +0700

    s

--- mylaravel/app/Http/Controllers/LoginController.php
+++ mylaravel/app/Http/Controllers/LoginController.php
@@ -9,4 +9,28 @@ class LoginController extends Controller
     function index() {
         return view('login');
     }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        // ตรวจสอบข้อมูลผู้ใช้ในฐานข้อมูล
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            // ถ้าผ่านการตรวจสอบให้เข้าสู่ระบบ
+            User::login($user);
+
+            // ลิงก์ไปที่หน้า home
+            return redirect()->route('home');
+        }
+
+        // ถ้าไม่ผ่านการตรวจสอบ
+        return back()->withErrors([
+            'email' => 'Invalid credentials.',
+        ]);
+    }
 }
