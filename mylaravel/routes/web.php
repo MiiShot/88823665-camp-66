1a0b4d5c211867d98d4ef23a652dc159d0bef3f4
MiiShot <kidrakon838084@gmail.com>
2025-02-26 22:56:48 +0700

    .

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -6,10 +6,34 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckLogin;
+use App\Http\Controllers\ProductController;
 
+
+Route::get('/product', [ProductController::class, 'index'])->middleware([CheckLogin::class]);
+Route::post('/product', [ProductController::class, 'add_product'])->middleware([CheckLogin::class]);
+
+Route::middleware([CheckLogin::class])->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/user/{id}', [UserController::class, 'edit']);
+    Route::put('/user', [UserController::class, 'edit_action']);
+    Route::delete('/user', [UserController::class, 'delete']);
+});
+
+Route::get('/',
+    [HomeController::class, 'index'])->middleware([CheckLogin::class]);
 
 Route::get('/login',
     [LoginController::class, 'index']);
+Route::post('/login',
+    [LoginController::class, 'login']);
+
+Route::get('/logout',function(){
+    session()->forget('user');
+    session()->flush();
+    return redirect('/login');
+});
+
 
 Route::get('/register',
     [RegisterController::class, 'index']);
@@ -20,7 +44,6 @@ Route::get('/home',
 Route::get('/',
     [HomeController::class, 'index']);
 
-
 Route::get('/', function () {
     return view('home');
 });
@@ -29,18 +52,8 @@ Route::post('/register',
     [RegisterController::class, 'create']);
 
 
-Route::get('/users',
-    [UserController::class, 'index']);
 
-Route::get('/user/{id}',
-    [UserController::class, 'edit']);
-
-Route::put('/user',
-    [UserController::class, 'edit_action']);
-
-Route::delete('/user',
-    [UserController::class, 'delete']);
-
+;
 
 
 /*
